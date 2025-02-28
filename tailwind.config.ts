88326458839da17ab243359f94e33bd7da78f806
Prince <princevirmani410@gmail.com>
2025-02-28 17:27:13 +0530

    deply

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				geist: ['Geist', 'sans-serif'],
+				geistMono: ['Geist Mono', 'monospace'],
+			},
 			colors: {
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
